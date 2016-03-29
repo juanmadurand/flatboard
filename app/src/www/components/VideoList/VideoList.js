f9b2588cc99dde7adad9fd596c297d93de8ad6da
Juanma Durand <juanma.durand@gmail.com>
2016-03-29 00:43:29 -0300

    Adding toggle view (list,grid) for Yt component

--- app/src/www/components/VideoList/VideoList.js
+++ app/src/www/components/VideoList/VideoList.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Image from 'react-bootstrap/lib/Image';
+import Thumbnail from 'react-bootstrap/lib/Thumbnail';
 import logger from 'www/helpers/logger';
 
 const styles = require('./VideoList.scss');
@@ -10,6 +11,7 @@ const videoUrlTemplate = 'https://www.youtube.com/watch?v=';
 export default class VideoList extends Component {
   static propTypes = {
     videos: PropTypes.array,
+    list: PropTypes.bool,
   };
 
   constructor(props) {
@@ -36,7 +38,7 @@ export default class VideoList extends Component {
               <h3>
                 <a href={videoUrl} title={snippet.title}>{snippet.title}</a>
               </h3>
-              <p> {snippet.description}</p>
+              <p>{snippet.description}</p>
             </div>
           </Col>
         </Row>
@@ -44,16 +46,45 @@ export default class VideoList extends Component {
     );
   }
 
+  renderVideoGrid(video) {
+    const snippet = video.snippet;
+    const {
+      url,
+      width,
+      height,
+    } = snippet.thumbnails.medium;
+
+    return (
+      <Col xs={6} md={4} className={styles.video_grid} key={video.id.videoId}>
+        <Thumbnail src={url} alt={`${width}x${height}`}>
+          <h3>{snippet.title}</h3>
+        </Thumbnail>
+      </Col>
+    );
+  }
+
   render() {
-    const { videos } = this.props;
+    const {
+      videos,
+      list,
+    } = this.props;
     if (!videos) {
       logger.error(`Failed to load ${this.constructor.name}`);
       return (<div />);
     }
+
+    if (list) {
+      return (
+        <div>
+          {videos.map(this.renderVideoList)}
+        </div>
+      );
+    }
+
     return (
-      <div>
-        {videos.map(this.renderVideoList)}
-      </div>
+      <Row>
+        {videos.map(this.renderVideoGrid)}
+      </Row>
     );
   }
 }
--- app/src/www/containers/Youtube/Youtube.js
+++ app/src/www/containers/Youtube/Youtube.js
@@ -1,6 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import debounce from 'lodash.debounce';
 import { load as videoSearch } from 'www/reducers/youtube';
+import { autobind } from 'core-decorators';
+
+import Well from 'react-bootstrap/lib/Well';
+import Button from 'react-bootstrap/lib/Button';
+
 import {
   VideoList,
   SearchBar,
@@ -8,9 +13,10 @@ import {
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { asyncConnect } from 'redux-async-connect';
 
 import { Loader } from 'www/components';
+
+const styles = require('./Youtube.scss');
 
 @connect(
   state => ({
@@ -19,11 +25,6 @@ import { Loader } from 'www/components';
   dispatch => bindActionCreators({ videoSearch }, dispatch)
 )
 
-@asyncConnect([{
-  promise: ({store: {dispatch, getState}}) => {
-  },
-}])
-
 export default class Youtube extends Component {
   static propTypes = {
     videos: PropTypes.object,
@@ -31,20 +32,48 @@ export default class Youtube extends Component {
     store: PropTypes.any,
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = { listMode: true };
+  }
+
+  @autobind
+  toggleViewMode() {
+    this.setState({listMode: !this.state.listMode});
+  }
+
   render() {
     const debounceSearch = debounce((term) => { this.props.videoSearch(term) }, 300);
     const {
       videos,
     } = this.props;
 
+    if (!this.state) {
+      return <div></div>;
+    }
+
     return (
-      <div>
-        <h3>Youtube search component</h3>
+      <Well>
+        <div className={styles.yt_header}>
+          <h3 className="pull-left">
+            <i className="fa fa-youtube" />
+             Search
+          </h3>
+          <div className="pull-right">
+            <Button bsStyle="default" onClick={this.toggleViewMode}>
+              {this.state.listMode ?
+                <i className="fa fa-th-list" /> :
+                <i className="fa fa-th-large" />
+              }
+            </Button>
+          </div>
+        </div>
         <SearchBar onSearchTermChange={debounceSearch} />
         {videos.loaded ?
-          <VideoList videos={videos.items} />
+          <VideoList videos={videos.items} list={this.state.listMode} />
         : <Loader />}
-      </div>
+      </Well>
     );
   }
 }
